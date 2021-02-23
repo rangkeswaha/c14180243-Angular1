0fd5b2153e2f3b94d4cb79ed7cdaeaed53efe77a
rangkeswaha <c14180243@john.petra.ac.id>
2021-02-23 20:26:25 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,58 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  player = true;
+  nama = "";
+  menang : Boolean = false;
+  koor1 = 0;
+  koor2 = 0;
+
+  satu = [
+    "*","*","*","*","*"
+  ];
+  dua = [
+    "*","*","*","*","*"
+  ];
+  tiga = [
+    "*","*","*","*","*"
+  ];
+  empat = [
+    "*","*","*","*","*"
+  ];
+  lima = [
+    "*","*","*","*","*"
+  ];
+
+  simbol = "";
+
+  check(){
+    if(this.player){
+      this.simbol = "1";
+    }else{
+      this.simbol = "0";
+    }
+
+    if (this.koor2 == 1){
+      this.satu[this.koor1 - 1] = this.simbol
+    }
+    else if (this.koor2 == 2){
+      this.dua[this.koor1 - 1] = this.simbol
+    }
+    else if (this.koor2 == 3){
+      this.tiga[this.koor1 - 1] = this.simbol
+    }
+    else if (this.koor2 == 4){
+      this.empat[this.koor1 - 1] = this.simbol
+    }
+    else if (this.koor2 == 5){
+      this.lima[this.koor1 - 1] = this.simbol
+    }
+
+    if(this.player){
+      this.player = false;
+    }else{
+      this.player = true;
+    }
+  }
+
 }
